ce3bc2a9e8ecc854302ed5aca90e48f1e03aa6df
Jorge Romeo <jromeosalazar@gmail.com>
2014-07-24 09:11:57 +0200

    Arreglar timeline Facebook y Twitter

--- src/Sylius/Bundle/WebBundle/Resources/public/js/main.js
+++ src/Sylius/Bundle/WebBundle/Resources/public/js/main.js
@@ -1,7 +1,7 @@
 // Carga los plugins de Foundation
 $(document).foundation();
 
-/******** FUNCION DE TWITTER *******************/
+/******** FUNCIÓN DE TWITTER *******************/
 !function(d,s,id){
 	var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
 	if(!d.getElementById(id)){
@@ -12,3 +12,13 @@ $(document).foundation();
 	}
 }
 (document,"script","twitter-wjs");
+
+/******** FUNCIÓN DE FACEBOOK *******************/
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&appId=755205047844171&version=v2.0";
+	fjs.parentNode.insertBefore(js, fjs);
+}
+(document, 'script', 'facebook-jssdk'));
